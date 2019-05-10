46ff2f0f3bd04d9d4ad0541c359c1900359d1d03
golf <golf_klub@hotmail.com>
2019-05-10 10:56:25 +0700

    fix vr camera position

--- src/WebVRScene.js
+++ src/WebVRScene.js
@@ -12,7 +12,7 @@ import { circleframe, logo } from "./component/Showroomlogo";
 import { config } from "./component/configWebVR";
 import { leftNavigate, rightNavigate } from "./component/NavigateButton";
 import { Toolbar } from "./component/toolbar";
-// import { WEBVR } from "./resources/controls/WebVR";
+import { WEBVR } from "./resources/controls/WebVR";
 import { DeviceOrientationControls } from "./resources/controls/DeviceOrientationControls";
 class App extends Component {
   polyfill = new WebVRPolyfill(config);
@@ -21,7 +21,6 @@ class App extends Component {
   componentDidMount() {
     this.sceneSetup();
     this.addCustomSceneObjects();
-    this.startAnimationLoop();
     window.addEventListener("resize", this.handleWindowResize);
   }
 
@@ -35,49 +34,45 @@ class App extends Component {
     );
     this.raycaster = new THREE.Raycaster();
     this.raycaster.setFromCamera({ x: 0, y: 0 }, this.camera);
-    this.camera.position.set(1.6, 0, -0.001);
-    // this.camera.position.y = 0;
-    // this.camera.position.x = 1;
-    // this.camera.position.z = -0.0001;
-    this.controls = new DeviceOrientationControls(this.camera);
-    // this.controls.enableZoom = false;
+
+    this.camera.position.z = -0.0001;
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setPixelRatio(window.devicePixelRatio);
-    // this.renderer.vr.enabled   = true;
 
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.mount.appendChild(this.renderer.domElement);
-    // document.body.appendChild(WEBVR.createButton(this.renderer));
+    document.body.appendChild(WEBVR.createButton(this.renderer));
 
-    // navigator.getVRDisplays().then(VRDisplay => {
-    //   if (VRDisplay.length) {
-    //     let vrDisplay = VRDisplay[0];
-    //     this.renderer.vr.enabled = true;
-    //     let controls = new DeviceOrientationControls(this.camera);
-    //     // this.controls = new OrbitControls(this.camera);
-    //     // this.controls.enableZoom = false;
-    //     vrDisplay.requestAnimationFrame(this.animate);
-    //     console.log("VR!");
-    //   } else {
-    //     console.log("DeskTop!");
-    //     let controls = new OrbitControls(this.camera);
-    //     controls.enableZoom = false;
-    //     controls.target.set(0, 1.6, 0.0001);
-    //     requestAnimationFrame(this.animate);
-    //   }
-    // });
-    // this.renderer.vr.enabled = true;
-    // this.renderer.setAnimationLoop(() => {
-    //   this.renderer.render(this.scene, this.camera);
-    // });
+    navigator.getVRDisplays().then(VRDisplay => {
+      if (VRDisplay.length) {
+        let vrDisplay = VRDisplay[0];
+        this.renderer.vr.enabled = true;
+        let controls = new DeviceOrientationControls(this.camera);
+        this.setState({ controls: controls });
+        vrDisplay.requestAnimationFrame(this.animate);
+        this.startAnimationLoop();
+        console.log("VR!");
+        this.camera.position.set(0, 0, -0.0001);
+        this.renderer.vr.enabled = true;
+      } else {
+        console.log("DeskTop!");
+        let controls = new OrbitControls(this.camera);
+        this.setState({ controls: controls });
+        controls.enableZoom = false;
+        // controls.target.set(0, 0, -0.0001);
+        requestAnimationFrame(this.animate);
+        this.startAnimationLoop();
+      }
+    });
+    this.renderer.setAnimationLoop(() => {
+      this.renderer.render(this.scene, this.camera);
+    });
   };
 
   addCustomSceneObjects = () => {
-    this.scene.add(this.controls);
     //Add content
     Content.map(res => this.scene.add(res));
-    this.scene.add(this.camera);
-    this.scene.add(this.controls);
+    // this.scene.add(this.camera);
     //Add logo button
     this.scene.add(logo);
     //Add circle showroom button // this.scene.add(curvedplane); //Add curved plane
@@ -93,7 +88,7 @@ class App extends Component {
   animate = () => {
     this.frameId = requestAnimationFrame(this.animate);
     this.renderer.render(this.scene, this.camera);
-    this.controls.update();
+    this.state.controls.update();
   };
 
   startAnimationLoop = () => !this.frameId && this.animate();

989cff0efd88cf6148d7a290b2fc42876a55fefc
golf <golf_klub@hotmail.com>
2019-05-14 11:32:49 +0700

    test somethings

--- src/resources/controls/DeviceOrientationControls.js
+++ src/resources/controls/DeviceOrientationControls.js
@@ -39,13 +39,13 @@ var DeviceOrientationControls = function(object) {
     var q1 = new THREE.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)); // - PI/2 around the x-axis
 
     return function(quaternion, alpha, beta, gamma, orient) {
-      // euler.set(beta, alpha, -gamma, "YXZ"); // 'ZXY' for the device, but 'YXZ' for us
+      euler.set(beta, alpha, -gamma, "YXZ"); // 'ZXY' for the device, but 'YXZ' for us
 
-      // quaternion.setFromEuler(euler); // orient the device
+      quaternion.setFromEuler(euler); // orient the device
 
-      // quaternion.multiply(q1); // camera looks out the back of the device, not the top
+      quaternion.multiply(q1); // camera looks out the back of the device, not the top
 
-      // quaternion.multiply(q0.setFromAxisAngle(zee, -orient)); // adjust for screen orientation
+      quaternion.multiply(q0.setFromAxisAngle(zee, -orient)); // adjust for screen orientation
     };
   })();
 
@@ -89,11 +89,11 @@ var DeviceOrientationControls = function(object) {
     if (device) {
       var alpha = device.alpha
         ? THREE.Math.degToRad(device.alpha) + scope.alphaOffset
-        : 0; // Z
+        : -1.6; // Z
 
       var beta = device.beta ? THREE.Math.degToRad(device.beta) : 0; // X'
 
-      var gamma = device.gamma ? THREE.Math.degToRad(device.gamma) : 0; // Y''
+      var gamma = device.gamma ? THREE.Math.degToRad(device.gamma) : -1.6; // Y''
 
       var orient = scope.screenOrientation
         ? THREE.Math.degToRad(scope.screenOrientation)

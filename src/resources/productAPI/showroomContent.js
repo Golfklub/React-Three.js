283bdfa8475cfbea74993eca92bce141e25e3454
golf <golf_klub@hotmail.com>
2019-05-22 10:42:49 +0700

    change image

--- src/resources/productAPI/showroomContent.js
+++ src/resources/productAPI/showroomContent.js
@@ -8,82 +8,6 @@ export const contentList = [
     },
     {
       x: -1.162,
-      src: "https://i.imgur.com/IzIdIpm.png",
-      z: -1.074,
-      rotation: 45
-    },
-    {
-      x: -1.536,
-      src: "https://i.imgur.com/IzIdIpm.png",
-      z: -0.414,
-      rotation: 75
-    },
-    {
-      x: 1.536,
-      src: "https://i.imgur.com/IzIdIpm.png",
-      z: -0.414,
-      rotation: -75
-    },
-    {
-      x: 1.168,
-      src: "https://i.imgur.com/IzIdIpm.png",
-      z: -1.109,
-      rotation: -45
-    },
-    {
-      x: 0.5,
-      src: "https://i.imgur.com/IzIdIpm.png",
-      z: -1.474,
-      rotation: -15
-    }
-  ],
-  [
-    {
-      x: -0.5,
-      src: "https://i.imgur.com/EDn1jl4.png",
-      z: -1.474,
-      rotation: 15
-    },
-    {
-      x: -1.162,
-      src: "https://i.imgur.com/EDn1jl4.png",
-      z: -1.074,
-      rotation: 45
-    },
-    {
-      x: -1.536,
-      src: "https://i.imgur.com/EDn1jl4.png",
-      z: -0.414,
-      rotation: 75
-    },
-    {
-      x: 1.536,
-      src: "https://i.imgur.com/EDn1jl4.png",
-      z: -0.414,
-      rotation: -75
-    },
-    {
-      x: 1.168,
-      src: "https://i.imgur.com/EDn1jl4.png",
-      z: -1.109,
-      rotation: -45
-    },
-    {
-      x: 0.5,
-      src: "https://i.imgur.com/EDn1jl4.png",
-      z: -1.474,
-      rotation: -15
-    }
-  ],
-  [
-    {
-      x: -0.5,
-      src: "https://i.imgur.com/IzIdIpm.png",
-      z: -1.474,
-      rotation: 15
-    },
-    {
-      x: -1.162,
       src: "https://i.imgur.com/EDn1jl4.png",
       z: -1.074,
       rotation: 45
@@ -109,6 +33,82 @@ export const contentList = [
     {
       x: 0.5,
       src: "https://i.imgur.com/fMCofVb.png",
+      z: -1.474,
+      rotation: -15
+    }
+  ],
+  [
+    {
+      x: -0.5,
+      src: "https://i.imgur.com/EgEuIHK.png",
+      z: -1.474,
+      rotation: 15
+    },
+    {
+      x: -1.162,
+      src: "https://i.imgur.com/5KWfJzw.png",
+      z: -1.074,
+      rotation: 45
+    },
+    {
+      x: -1.536,
+      src: "https://i.imgur.com/ZXuaUTj.png",
+      z: -0.414,
+      rotation: 75
+    },
+    {
+      x: 1.536,
+      src: "https://i.imgur.com/jU9sWiV.png",
+      z: -0.414,
+      rotation: -75
+    },
+    {
+      x: 1.168,
+      src: "https://i.imgur.com/nIgltob.png",
+      z: -1.109,
+      rotation: -45
+    },
+    {
+      x: 0.5,
+      src: "https://i.imgur.com/4cAV8S4.png",
+      z: -1.474,
+      rotation: -15
+    }
+  ],
+  [
+    {
+      x: -0.5,
+      src: "https://i.imgur.com/NYh8DSM.png",
+      z: -1.474,
+      rotation: 15
+    },
+    {
+      x: -1.162,
+      src: "https://i.imgur.com/WJXIJrE.png",
+      z: -1.074,
+      rotation: 45
+    },
+    {
+      x: -1.536,
+      src: "https://i.imgur.com/E59SmJT.png",
+      z: -0.414,
+      rotation: 75
+    },
+    {
+      x: 1.536,
+      src: "https://i.imgur.com/ZujwIsi.png",
+      z: -0.414,
+      rotation: -75
+    },
+    {
+      x: 1.168,
+      src: "https://i.imgur.com/k5zAkQS.png",
+      z: -1.109,
+      rotation: -45
+    },
+    {
+      x: 0.5,
+      src: "https://i.imgur.com/X4HywVa.png",
       z: -1.474,
       rotation: -15
     }

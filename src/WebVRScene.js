b976b0fc256bfa1c038b5e9d84831f2c1869464e
golf <golf_klub@hotmail.com>
2019-05-23 11:27:47 +0700

    fix camera position for android

--- src/WebVRScene.js
+++ src/WebVRScene.js
@@ -68,7 +68,7 @@ class App extends Component {
         this.setState({ controls: controls, device: "vr" });
         vrDisplay.requestAnimationFrame(this.animate);
         this.startAnimationLoop();
-        // sphereInside.rotation.set(0, controls.object.rotation.y, 0, "XYZ");
+        sphereInside.rotation.set(0, 0, 0, "XYZ");
       } else {
         let controls = new OrbitControls(this.camera, this.renderer.domElement);
         this.setState({ controls: controls, device: "desktop" });

707d06f70228ebe3ac6b3e0cc5d93801cccb32d8
golf <golf_klub@hotmail.com>
2019-05-23 18:59:10 +0700

    headset mode content position[android]

--- src/resources/controls/WebVR.js
+++ src/resources/controls/WebVR.js
@@ -40,19 +40,21 @@ export const WEBVR = {
             console.log(screen.orientation.type);
             sphereInside.rotation.set(
               0,
-              controls.object.rotation.y - 1.57,
+              controls.object.rotation.y + 1.57,
               0,
               "XYZ"
             );
+            sphereAngle.rotation.set(0, 0, 0, "XYZ");
           } else {
             console.log(controls);
             console.log(screen.orientation.type);
             sphereInside.rotation.set(
               0,
-              controls.object.rotation.y - 1.57,
+              controls.object.rotation.y + 1.57,
               0,
               "XYZ"
             );
+            sphereAngle.rotation.set(0, 0, 0, "XYZ");
             // sphereInside.rotation.set(controls.object.rotation.x, 0, 0, "XYZ");
             // sphereAngle.rotation.set(0, controls.object.rotation.y, 0, "XYZ");
           }

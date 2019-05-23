cd3d6b405a41e47b0a388c77098277d2f4b5c121
golf <golf_klub@hotmail.com>
2019-05-23 18:14:35 +0700

    golf

--- src/component/Recenter.js
+++ src/component/Recenter.js
@@ -24,7 +24,7 @@ export const Recenter = (renderer, controls, type) => {
         controls.object.position.y = 1.6;
       } else if (type === "vr") {
         sphereInside.rotation.set(controls.object.rotation.x, 0, 0, "XYZ");
-        sphereAngle.rotation.set(0, controls.object.rotation.y, 0, "XYZ");
+        // sphereAngle.rotation.set(0, controls.object.rotation.y, 0, "XYZ");
       }
     };
     renderer.vr.setDevice(device);
